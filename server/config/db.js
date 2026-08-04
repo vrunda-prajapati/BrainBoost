20779b2edb80a122342bfb2554e74405008a56e4
vrunda-prajapati <vrundaprajapati0204@gmail.com>
2026-08-04 20:52:12 +0530

    Fix authMiddleware filename

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,10 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect((err) => {
   if (err) {
-    console.error("Database Connection Failed");
+    console.error("❌ Database Connection Failed");
     console.error(err);
   } else {
     console.log("✅ MySQL Connected Successfully");
