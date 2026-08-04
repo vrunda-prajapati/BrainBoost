245f48a1a39188d5bdee66254a5124b294123359
vrunda-prajapati <vrundaprajapati0204@gmail.com>
2026-08-04 20:57:36 +0530

    Fix Railway MySQL configuration

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,12 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+});
 
 db.connect((err) => {
   if (err) {
